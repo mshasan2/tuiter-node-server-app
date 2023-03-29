15462c447b9fd742b0197dfb198229a7d16e74bd
mshasan2 <55490970+mshasan2@users.noreply.github.com>
2023-03-29 15:23:58 -0400

    first commit

---
+++ controllers/tuits/tuits-controller.js
@@ -0,0 +1,48 @@
+import posts from './tuits.js';
+let tuits = posts;
+
+const createTuit = (req, res) => {
+    const newTuit = req.body;
+    newTuit._id = (new Date()).getTime()+'';
+    newTuit.likes = 0;
+    newTuit.liked = false;
+    newTuit.handle = "@NASA";
+    newTuit.replies = 0;
+    newTuit.retuits = 0;
+    newTuit.userName = "NASA";
+    newTuit.time = "1m";
+    newTuit.image = "nasa.jpeg";
+    newTuit.topic = "Space";
+    newTuit.dislikes = 0;
+    newTuit.title = newTuit.tuit;
+    tuits.push(newTuit);
+    res.json(newTuit);
+}
+const findTuits  = (req, res) => {
+    res.json(tuits);
+}
+const updateTuit = (req, res) => {
+    const tuitdIdToUpdate = parseInt(req.params.tid);
+    const updates = req.body;
+    console.log(tuitdIdToUpdate);
+    const tuitIndex = tuits.findIndex(
+        (t) => t._id === tuitdIdToUpdate )
+    console.log(tuitIndex)
+    tuits[tuitIndex] = {...tuits[tuitIndex], ...updates};
+    // console.log(tuits);
+    console.log(tuits[tuitIndex]);
+    res.sendStatus(200);
+}
+
+const deleteTuit = (req, res) => {
+    const tuitId = req.params.tid;
+    tuits = tuits.filter((tuit) => tuit._id !== tuitId);
+    res.sendStatus(200);
+}
+
+export default (app) => {
+    app.post('/api/tuits', createTuit);
+    app.get('/api/tuits', findTuits);
+    app.put('/api/tuits/:tid', updateTuit);
+    app.delete('/api/tuits/:tid', deleteTuit);
+}
